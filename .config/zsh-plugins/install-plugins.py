e866b453125c8a4e892ba6f82b33b6860e1c232b
Aidan Shilling <aidanshilling91@gmail.com>
2023-10-05 23:53:59 -0500

    updated zsh plugin script

--- .config/zsh-plugins/install-plugins.py
+++ .config/zsh-plugins/install-plugins.py
@@ -3,6 +3,10 @@ import os
 with open('./plugins', 'r') as f:
     for line in f:
         name, repo = line.split(": ")
-        print("Installing " + name + "...")
-        os.system('git clone ' + repo)
+        if not os.path.exists('./' + name):
+            print("Installing " + name + "...")
+            os.system('git clone ' + repo)
+        else:
+            print(name + " already installed, skipping...")
+    print("Done.")
 
